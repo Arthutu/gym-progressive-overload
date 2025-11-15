10eae45f88ea56c038eeb9550c1d3ea7c28ab269
Arthur Rodolfo <arthurrodolfo81@gmail.com>
2025-11-15 10:01:53 -0800

    initial commit

---
+++ gym-progressive-overload/gym_progressive_overloadApp.swift
@@ -0,0 +1,32 @@
+//
+//  gym_progressive_overloadApp.swift
+//  gym-progressive-overload
+//
+//  Created by Arthur Rodolfo on 2025-11-15.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct gym_progressive_overloadApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
